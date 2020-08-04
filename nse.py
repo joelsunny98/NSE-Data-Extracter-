5f3fcc20ef7fa4e3d46b8e9bf3e8e5489b4a9fed
Joel Sunny <joelsunnygeorge@gmail.com>
2020-08-04 07:09:57 +0530

    Created new funtion to extract the data

--- nse.py
+++ nse.py
@@ -6,21 +6,31 @@ from datetime import datetime
 from datetime import date
 from datetime import timedelta
 
-#URL for PWOI is initialised
-url = "https://www1.nseindia.com/content/nsccl/fao_participant_oi_08072020.csv"
+def extract(day):
+    '''
+    DESCRIPTION: Extracts data for the Participant wise Open Interest(PWOI)
+    data from the NSE Website and updates it onto an excel file.
+    INPUT: The CSV file from which you want the data
+    OUTPUT: Null, it does not return any result but stores the extracted data
+    on to the Excel Sheet.
+    '''
+    #URL For PWOI is initialised
+    url = "https://www1.nseindia.com/content/nsccl/fao_participant_oi_08072020.csv"
+    # The URL is changed accordingly to the required day
+    new_url = url[0:59] + day + url[67: ]
 
-#find the current date
-now = datetime.now()
-#convert date to a format ready to put into URL
-today = now.strftime("%d%m%Y")
+    #CSV file for that day is extracted
+    data = pd.DataFrame(pd.read_csv(new_url))
+    # the data already on the excel sheet is extracted
+    sheet = pd.DataFrame(pd.read_excel('data.xlsx'))
+    
+    #The new data is appended to the data on the excel sheet
+    updated_sheet = sheet.append(data)
 
-#The url is changed according to the day
-new_url = url[0:59] + today + url[67: ]
+    # The updated data is stored on the excel sheet
+    updated_sheet.to_excel('data.xlsx')
+    print("...DATA EXTRACTION COMPLETE")
+    print("Please check the file: data.xlsx")
 
-#CSV file for that day is exctracted
-data = pd.DataFrame(pd.read_csv(new_url))
-#the data already on the excel sheet is extracted
-sheet = pd.DataFrame(pd.read_excel('data.xlsx'))
 
-#The new data is appended to the data on the Excel Sheet
-updated_sheet = sheet.append(data)
+
