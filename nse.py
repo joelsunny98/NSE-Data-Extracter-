787ac368a718c4290319e62e99e640005e1cc413
Joel Sunny <joelsunnygeorge@gmail.com>
2020-08-04 07:23:12 +0530

    Added new function pre_date() to find the previous day's date

--- nse.py
+++ nse.py
@@ -6,6 +6,34 @@ from datetime import datetime
 from datetime import date
 from datetime import timedelta
 
+def pre_date():
+    '''
+    DESCRIPTION: This function will return the previous day's date
+    INPUT: Null
+    OUTPUT: Previous day's date in format ready to be used in URL
+    '''
+    # Get today's name in number
+    myday = datetime.weekday(date.today())
+
+    # Get today's date
+    today = date.today()
+
+    #Yesterday's date
+    yesterday = str(today - timedelta(days = 1))
+
+    # Extractting day month and year individually
+    day = yesterday[8: ]
+    month = yesterday[5:7]
+    year = yesterday[0:4]
+
+    # Finding out if the previous day was a weekend for which there is no data
+    if myday == 0 or myday == 6:
+        print("The market was closed Yesterday")
+    else:
+        # The market are open on weekdays
+        # The date is return in format that can be put inbetween the URL
+        return day + month + year
+
 def extract(day):
     '''
     DESCRIPTION: Extracts data for the Participant wise Open Interest(PWOI)
@@ -31,6 +59,16 @@ def extract(day):
     updated_sheet.to_excel('data.xlsx')
     print("...DATA EXTRACTION COMPLETE")
     print("Please check the file: data.xlsx")
+
+    #Find the current date
+    now = datetime.now()
+    # Convert data into a format ready to be used in the url
+    today = now.strftime("%d%m%Y")
+
+    try: 
+        extract(today)
+    except:
+        print("There is an error")
 
 
 
